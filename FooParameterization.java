6ac8c39567c911711287a1223a7a70eae5af428a
Jacce522 <70173113+Jacce522@users.noreply.github.com>
2024-06-04 14:41:18 -0500

    Create FooParameterization.java
    Added a new file for the code to calculate the volume of a sphere

---
+++ FooParameterization.java
@@ -0,0 +1,14 @@
+package spherePackage;
+
+public class FooParameterization 
+
+{
+	public static double calculateSphereVolume(double radius) 
+    {
+        return (4.0/3.0) * Math.PI * Math.pow(radius, 3); //Does the calculation
+    }
+	
+	//plenty of room for further development
+	
+	
+}
